4ac61c9239902f80222083ff3413ee6d5f45ccf4
nishant <nishant.singh87@gmail.com>
2018-02-11 12:25:44 +0530

    Pending for missing steps.

--- features/steps/users.rb
+++ features/steps/users.rb
@@ -1,12 +1,16 @@
 Given(/^I open zinc homepage$/) do
+  pending
 end
 
 
 Then(/^I click on login$/) do
+  pending
 end
 
 And(/^I enter username and password for admin user$/) do
+  pending
 end
 
 Then(/^I see homepage with site admin link in top bar$/) do
+  pending
 end
