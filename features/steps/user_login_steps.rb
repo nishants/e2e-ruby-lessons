49431c8db02b9feabf47d66c981ec0f09e3055a6
nishant <nishant.singh87@gmail.com>
2018-02-11 11:53:09 +0530

    restore lesson 01

--- features/steps/user_login_steps.rb
+++ features/steps/user_login_steps.rb
@@ -1,16 +1,19 @@
 Given(/^I open zinc homepage$/) do
-  visit(HomePage)
+  @browser.goto "http://staging.zinclearninglabs.com/"
 end
 
+
 Then(/^I click on login$/) do
-  on(HomePage).click_login
+  @browser.element(:text => 'Login').click
+end
+
+And(/^I enter username and password for admin user$/) do
+  @browser.text_field(:id => 'user_email').set('karthik.cs@gmail.com')
+  @browser.text_field(:id => 'user_password').set('password')
+  @browser.button(:text => 'Sign in').click
 end
 
 Then(/^I see homepage with site admin link in top bar$/) do
-  expect(on(HomePage).site_admin_link_visible?).to eq(true)
-end
-
-And(/^I enter username and password for "([^"]*)" user$/) do |user|
-  user = @users.get(user)
-  on(LoginPage).login_with(user)
+  site_admin_link = @browser.element(:text => '(Site Admin)')
+  expect(site_admin_link.visible?).to eq(true)
 end
--- features/user-login.feature
+++ features/user-login.feature
@@ -5,5 +5,5 @@ Feature: Login as zinc user
   Scenario: Login as zinc admin
     Given I open zinc homepage
     Then  I click on login
-    And   I enter username and password for "admin" user
+    And   I enter username and password for admin user
     Then  I see homepage with site admin link in top bar
