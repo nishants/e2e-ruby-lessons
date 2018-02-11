68f6848bd2637a289a2141735440fbf13e446725
nishant <nishant.singh87@gmail.com>
2018-02-11 11:50:49 +0530

    remove unused files

--- lib/pages/login_page.rb
+++
@@ -1,8 +0,0 @@
-class LoginPage < Page
-
-  def login_with(user)
-    @browser.text_field(:id => 'user_email').set('karthik.cs@gmail.com')
-    @browser.text_field(:id => 'user_password').set('password')
-    @browser.button(:text => 'Sign in').click
-  end
-end
