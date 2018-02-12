4a46a1ef10e9c297e6d4f421a7bcd365b94e078e
nishant <nishant.singh87@gmail.com>
2018-02-12 18:16:44 +0530

    Test for student user.

--- features/steps/users.rb
+++ features/steps/users.rb
@@ -17,3 +17,21 @@ Then(/^I see homepage with site admin link in top bar$/) do
   site_admin_link = @browser.element(:text => '(Site Admin)')
   expect(site_admin_link.visible?).to eq(true)
 end
+
+And(/^I can open admin section$/) do
+  @browser.element(:text => '(Site Admin)').click
+  @browser.element(:class => 'administration_namespace').visible?
+end
+
+
+And(/^I enter username and password for student user$/) do
+  @browser.text_field(:id => 'user_email').set('86.rohit+1@gmail.com')
+  @browser.text_field(:id => 'user_password').set('password')
+  @browser.button(:text => 'Sign in').click
+end
+
+
+Then(/^I see homepage without admin link in top bar$/) do
+  site_admin_link = @browser.element(:text => '(Site Admin)')
+  expect(site_admin_link.exists?).to eq(false)
+end
--- features/users.feature
+++ features/users.feature
@@ -7,3 +7,11 @@ Feature: Login as zinc user
     Then  I click on login
     And   I enter username and password for admin user
     Then  I see homepage with site admin link in top bar
+    And   I can open admin section
+
+  @student
+  Scenario: Login as zinc student
+    Given I open zinc homepage
+    Then  I click on login
+    And   I enter username and password for student user
+    Then  I see homepage without admin link in top bar
