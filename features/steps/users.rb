b70ff030af20e59ac10ed8595433fc9a74902cec
fagunbhavsar <github.fagun@gmail.com>
2018-02-16 15:11:32 +0530

    - Removed hard-coded user accounts' details

--- features/steps/users.rb
+++ features/steps/users.rb
@@ -7,7 +7,8 @@ Then(/^I click on login$/) do
 end
 
 And(/^I enter username and password for admin user$/) do
- 	on(LoginForm).login("fagun@zinclearninglabs.com", "Fagun@456")
+  user = @users.get("fagun-site-admin")
+ 	on(LoginForm).login(user["username"], user["password"])
 end
 
 Then(/^I see homepage with site admin link in top bar$/) do
@@ -16,7 +17,8 @@ Then(/^I see homepage with site admin link in top bar$/) do
 end
 
 And(/^I enter username and password for student user$/) do
-	on(LoginForm).login("fagun.bhavsar@gmail.com", "Fagun@456")
+  user = @users.get("fagun-student")
+	on(LoginForm).login(user["username"], user["password"])
 end
 
 And(/^I do not see site admin link and admin button$/) do
